4f98ab40078867fa0eb106dc8716397e41c6a52d
山崎匠真 <yamasakit3a@aquila-wl.imp>
2024-04-24 23:41:22 +0900

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from 'react-native-webview'
 
+const OhGireal ='https://oh-gireal.vercel.app/'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{width:'100%',height:'100%'}}> 
+        <WebView
+          source={{ uri: OhGireal }}
+          onLoad={console.log('loaded')}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
